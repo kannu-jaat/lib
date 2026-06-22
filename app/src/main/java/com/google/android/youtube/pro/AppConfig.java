e54b20788d38552faa7d17c7922dbaff38cffc3c
kannu-jaat <krix7096@gmail.com>
2026-06-22 13:52:56 +0530

    Add AppConfig class with library name and contact email

---
+++ app/src/main/java/com/google/android/youtube/pro/AppConfig.java
@@ -0,0 +1,10 @@
+package com.google.android.youtube.pro;
+
+public class AppConfig {
+    // Kisi aur library ko sell karte waqt bas yahan naam badal dena
+    public static final String LIBRARY_NAME = "KRISHNA LIBRARY";
+    
+    // Future ke liye (jaise Admin ka UPI ID, etc.) hum yahan add karte rahenge
+    public static final String CONTACT_EMAIL = "admin@krishnalibrary.com";
+}
+
